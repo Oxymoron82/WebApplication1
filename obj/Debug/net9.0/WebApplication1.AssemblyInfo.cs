73ed6b7700e5da2a3de9b165ad94da40fcadf702
Oxymoron82 <oxumoron82@gmail.com>
2025-06-01 17:45:36 +0300

    Add project files

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9cbfb0eeffe9fedc9c38e75ed32ed524bb71eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a988f0338959d3771469c2b8d7604040869ad3b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
